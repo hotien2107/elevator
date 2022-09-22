b917ac15eed59f32ad752fc4abacc2af7aed4311
hotien2107 <hotien2107@gmail.com>
2022-09-22 23:38:56 +0700

    update header

--- layouts/Header/index.tsx
+++ layouts/Header/index.tsx
@@ -1,13 +1,35 @@
-import React from "react";
-import Introduction from "../../components/pages/home/Introduction";
+import React, { useEffect, useRef, useState } from "react";
 import Container from "../Container";
 import HeaderMenu from "./components/HeaderMenu";
+import Logo from "../../assets/images/logo.png";
+import Image from "next/image";
 
 const Header = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div className="header">
-      <div className="header__logo">Hung Phu Gia</div>
-      <HeaderMenu />
+    <div
+      className={"header " + (offset >= 10 ? "header--black" : "")}
+      ref={ref}
+    >
+      <Container className="header__container">
+        <Image
+          src={Logo}
+          alt="Công ty thang máy gia đình Hưng Phú Gia"
+          className="header__logo"
+        />
+        <HeaderMenu />
+      </Container>
     </div>
   );
 };
